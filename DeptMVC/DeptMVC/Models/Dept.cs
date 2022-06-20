2200b1bda6f55c63c7bd2fc7e6ba37aa4d8de04d
Akshay Shinde <a.shinde1@zensar.com>
2022-06-20 12:15:13 +0530

    Asp.net WebApp-MVC

---
+++ DeptMVC/DeptMVC/Models/Dept.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeptMVC.Models
+{
+    public class Dept
+    {
+        public int Id { get; set; }
+        public string DeptName { get; set; }
+        public string DeptLocation { get; set; }
+        public string Branches { get; set; }
+    }
+}
